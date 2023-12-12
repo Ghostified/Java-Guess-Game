ba62482ed297ba95375aa22249fa4a92289d8c18
Allan Branson <bransonaallan@gmail.com>
2023-12-12 14:47:47 +0300

    Println()VSPrint()

---
+++ Hi.java
@@ -0,0 +1,8 @@
+public class Hi {
+    public static void main (String [] args){
+        System.out.println("Hello Java, Here we are again");
+        System.out.print("Okay, this is something new. ");
+        System.out.println("Hello Java, Here we are again");
+        System.out.print("Okay, this is something new ");
+    }
+}
