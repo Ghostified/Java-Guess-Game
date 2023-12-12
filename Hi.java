34da6f07e386905360896113e5b6658fb6d22c25
Allan Branson <bransonaallan@gmail.com>
2023-12-12 14:50:58 +0300

    printline functions

--- Hi.java
+++ Hi.java
@@ -4,5 +4,10 @@ public class Hi {
         System.out.print("Okay, this is something new. ");
         System.out.println("Hello Java, Here we are again");
         System.out.print("Okay, this is something new ");
+        System.out.println(" ");
+        System.out.println(3 + 3);
+        System.out.println(79);
+        System.out.println(30*50);
+
     }
 }
